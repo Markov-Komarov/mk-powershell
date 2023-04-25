9e4d199855c188c3bf6f55a107008bd3f064eff4
Markov <83531009+Markov-Komarov@users.noreply.github.com>
2023-04-24 22:36:25 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# mk-powershell
+My personal configuration and customization for PowerShell.
+Pleas note that this is based on [ChrisTitusTech Powershell](https://github.com/ChrisTitusTech/powershell-profile/) profile.
+It has been modified by me for my personal usage and liking.
+
+The main change it's the prompt preference, changing it from Oh-My-Posh to Starship for better performance and shorter loading times.
+Unused fuctions have been removed.
+
+## One Command Install (Admin PowerShell Recommended)
+
+Execute the following command in an elevated PowerShell window to install the PowerShell profile:
+
+```
+irm https://raw.githubusercontent.com/Markov-Komarov/mk-profile/main/setup.ps1" | iex
+```
+
+## Fix the Missing Font
+
+After running the script, you'll find a downloaded `cove.zip` file in the folder you executed the script from. Follow these steps to install the required nerd fonts:
+
+1. Extract the `cove.zip` file.
+2. Locate and install the nerd fonts.
+
+Enjoy a minimalistic and enhanced Powershell experience!
---
+++ setup.ps1
@@ -0,0 +1,52 @@
+#If the file does not exist, create it.
+if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
+    try {
+        # Detect Version of Powershell & Create Profile directories if they do not exist.
+        if ($PSVersionTable.PSEdition -eq "Core" ) { 
+            if (!(Test-Path -Path ($env:userprofile + "\Documents\Powershell"))) {
+                New-Item -Path ($env:userprofile + "\Documents\Powershell") -ItemType "directory"
+            }
+        }
+        elseif ($PSVersionTable.PSEdition -eq "Desktop") {
+            if (!(Test-Path -Path ($env:userprofile + "\Documents\WindowsPowerShell"))) {
+                New-Item -Path ($env:userprofile + "\Documents\WindowsPowerShell") -ItemType "directory"
+            }
+        }
+
+        Invoke-RestMethod https://raw.githubusercontent.com/Markov-Komarov/mk-powershell/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+        Write-Host "The profile @ [$PROFILE] has been created."
+		
+		Invoke-WebRequest https://raw.githubusercontent.com/Markov-Komarov/mk-powershell/main/starship.toml -o "$HOME\Documents\PowerShell\starship.toml"
+		Write-Host "The starship config @ [$env:userprofile + "\Documents\Powershell"] has been created."
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+}
+# If the file already exists, show the message and replace it.
+ else {
+		 Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
+		 Invoke-RestMethod https://raw.githubusercontent.com/Markov-Komarov/mk-powershell/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+		 
+		 Invoke-WebRequest https://raw.githubusercontent.com/Markov-Komarov/mk-powershell/main/starship.toml -o "$HOME\Documents\PowerShell\starship.toml"
+		 Write-Host "The starship config @ [$env:userprofile + "\Documents\Powershell"] has been created."
+ }
+& $profile
+
+# Font Install
+# You will have to extract and Install this font manually
+# You will also need to set your Nerd Font of choice in your window defaults or in the Windows Terminal Settings.
+Invoke-RestMethod https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip?WT.mc_id=-blog-scottha -o cove.zip
+
+# Choco install
+#
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Starship Install
+#
+choco install starship
+
+# Terminal Icons Install
+#
+Install-Module -Name Terminal-Icons -Repository PSGallery
